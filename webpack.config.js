7589eaf0eda85995b77a96ded6db52a617355235
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-10-14 12:24:16 +0200

    update

--- webpack.config.js
+++ webpack.config.js
@@ -12,8 +12,7 @@ Repositories: https://gitlab.in2p3.fr/ami-team/AMIMQTTClientJS/
               https://www.github.com/ami-team/AMIMQTTClientJS/
 
 This software is a computer program whose purpose is to provide an
-Eclipse Paho-based MQTT Client to the ATLAS Metadata Interface (AMI)
-ecosystem.
+MQTT Client to the ATLAS Metadata Interface (AMI) ecosystem.
 
 This software is governed by the CeCILL-C license under French law and
 abiding by the rules of distribution of free software. You can use, 
@@ -23,7 +22,6 @@ license as circulated by CEA, CNRS and INRIA at the following URL
 
 The fact that you are presently reading this means that you have had
 knowledge of the CeCILL-C license and that you accept its terms.
-
 `
 
 /*--------------------------------------------------------------------------------------------------------------------*/
