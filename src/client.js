0c500ff28cd774abccc210d54cf8c25464d71246
Jérôme Odier <jerome.odier@lpsc.in2p3.fr>
2021-11-04 02:06:15 +0100

    update

--- src/client.js
+++ src/client.js
@@ -33,15 +33,11 @@ function parseJwt(token)
 			/* DECODE PAYLOAD                                                                                         */
 			/*--------------------------------------------------------------------------------------------------------*/
 
-			const uriEncodedPayload = atob(parts[1].replace(/-/g, '+').replace(/_/g, '/')).split('').map((c) => {
+			const payload = decodeURIComponent(atob(parts[1].replace(/-/g, '+').replace(/_/g, '/')).split('').map((c) => {
 
 				return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
 
-			}).join('');
-
-			/*--------------------------------------------------------------------------------------------------------*/
-
-			const payload = decodeURIComponent(uriEncodedPayload);
+			}).join(''));
 
 			/*--------------------------------------------------------------------------------------------------------*/
 			/* PARSE PAYLOAD                                                                                          */
